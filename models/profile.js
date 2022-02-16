8fb66e6d25f1ac48c39d6ea7f95301dc0fe4de7c
ashleyromano <ashleye.romano@gmail.com>
2022-02-16 14:20:43 -0500

    basic back end

--- models/profile.js
+++ models/profile.js
@@ -1,12 +1,16 @@
 import mongoose from 'mongoose'
 
 const profileSchema = new mongoose.Schema({
-  email: {type: String, required: true, lowercase: true, unique: true},
+  email: { type: String, required: true, lowercase: true, unique: true },
   name: String,
-},{
-    timestamps: true,
+  events: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Event'
+  }]
+}, {
+  timestamps: true,
 })
 
 const Profile = mongoose.model('Profile', profileSchema)
 
-export {Profile}
+export { Profile }
